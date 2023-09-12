81e17ab99bbc8b4a51b0509bb1c184995e1ecb75
Soorya U <sooryau7@gmail.com>
2023-09-12 10:22:59 +0530

    Adding Files

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { ApiserviceService } from 'src/app/services/apiservice.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+  
+export class HomeComponent {
+
+  usdprices:any;
+  inrprices:any;
+
+  constructor(private api:ApiserviceService){
+    this.api.getUsdPrices().subscribe(res=>{
+      this.usdprices=res;
+    });
+    this.api.getInrPrices().subscribe(res=>{
+      this.inrprices=res;
+    }); 
+  }
+}
