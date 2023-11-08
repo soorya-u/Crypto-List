e7cf77bfc1fd511614ab24923265b93002ddf5df
Soorya U <sooryau7@gmail.com>
2023-11-08 14:39:03 +0530

    Added Indent

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,20 +4,18 @@ import { ApiserviceService } from 'src/app/services/apiservice.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-  
 export class HomeComponent {
+  usdprices: any;
+  inrprices: any;
 
-  usdprices:any;
-  inrprices:any;
-
-  constructor(private api:ApiserviceService){
-    this.api.getUsdPrices().subscribe(res=>{
-      this.usdprices=res;
+  constructor(private api: ApiserviceService) {
+    this.api.getUsdPrices().subscribe((res) => {
+      this.usdprices = res;
     });
-    this.api.getInrPrices().subscribe(res=>{
-      this.inrprices=res;
-    }); 
+    this.api.getInrPrices().subscribe((res) => {
+      this.inrprices = res;
+    });
   }
 }
